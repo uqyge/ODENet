30936f68408c5c3d7c7830240820c3480a1552f6
eg <y.ge1222@gmail.com>
2020-08-30 16:12:57 +0200

    add k2pb

--- ODENet/utils.py
+++ ODENet/utils.py
@@ -1,31 +1,36 @@
 import math
 
 import numpy as np
+import tensorflow as tf
 import tensorflow.keras.backend as K
 from tensorflow.keras.callbacks import Callback, LearningRateScheduler
+from tensorflow.python.framework.convert_to_constants import (
+    convert_variables_to_constants_v2,
+)
 
 
 def cubic_loss(y_true, y_pred):
-    return K.mean(K.square(y_true - y_pred)*K.abs(y_true - y_pred), axis=-1)
+    return K.mean(K.square(y_true - y_pred) * K.abs(y_true - y_pred), axis=-1)
 
 
 def coeff_r2(y_true, y_pred):
     from keras import backend as K
-    SS_res = K.sum(K.square(y_true-y_pred))
+
+    SS_res = K.sum(K.square(y_true - y_pred))
     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
-    return (1 - SS_res/(SS_tot + K.epsilon()))
+    return 1 - SS_res / (SS_tot + K.epsilon())
 
 
 def step_decay(epoch):
     initial_lrate = 0.001
     drop = 0.5
     epochs_drop = 200.0
-    lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+    lrate = initial_lrate * math.pow(drop, math.floor((1 + epoch) / epochs_drop))
     return lrate
 
 
 class SGDRScheduler(Callback):
-    '''Cosine annealing learning rate scheduler with periodic restarts.
+    """Cosine annealing learning rate scheduler with periodic restarts.
     # Usage
         ```python
             schedule = SGDRScheduler(min_lr=1e-5,
@@ -47,15 +52,17 @@ class SGDRScheduler(Callback):
     # References
         Blog post: jeremyjordan.me/nn-learning-rate
         Original paper: http://arxiv.org/abs/1608.03983
-    '''
+    """
 
-    def __init__(self,
-                 min_lr,
-                 max_lr,
-                 steps_per_epoch,
-                 lr_decay=1,
-                 cycle_length=10,
-                 mult_factor=2):
+    def __init__(
+        self,
+        min_lr,
+        max_lr,
+        steps_per_epoch,
+        lr_decay=1,
+        cycle_length=10,
+        mult_factor=2,
+    ):
 
         self.min_lr = min_lr
         self.max_lr = max_lr
@@ -72,24 +79,24 @@ class SGDRScheduler(Callback):
         self.history = {}
 
     def clr(self):
-        '''Calculate the learning rate.'''
-        fraction_to_restart = self.batch_since_restart / \
-            (self.steps_per_epoch * self.cycle_length)
-        lr = self.min_lr + 0.5 * \
-            (self.max_lr - self.min_lr) * \
-            (1 + np.cos(fraction_to_restart * np.pi))
+        """Calculate the learning rate."""
+        fraction_to_restart = self.batch_since_restart / (
+            self.steps_per_epoch * self.cycle_length
+        )
+        lr = self.min_lr + 0.5 * (self.max_lr - self.min_lr) * (
+            1 + np.cos(fraction_to_restart * np.pi)
+        )
         return lr
 
     def on_train_begin(self, logs={}):
-        '''Initialize the learning rate to the minimum value at the start of training.'''
+        """Initialize the learning rate to the minimum value at the start of training."""
         logs = logs or {}
         K.set_value(self.model.optimizer.lr, self.max_lr)
 
     def on_batch_end(self, batch, logs={}):
-        '''Record previous batch statistics and update the learning rate.'''
+        """Record previous batch statistics and update the learning rate."""
         logs = logs or {}
-        self.history.setdefault('lr', []).append(
-            K.get_value(self.model.optimizer.lr))
+        self.history.setdefault("lr", []).append(K.get_value(self.model.optimizer.lr))
         for k, v in logs.items():
             self.history.setdefault(k, []).append(v)
 
@@ -97,7 +104,7 @@ class SGDRScheduler(Callback):
         K.set_value(self.model.optimizer.lr, self.clr())
 
     def on_epoch_end(self, epoch, logs={}):
-        '''Check for end of current cycle, apply restarts when necessary.'''
+        """Check for end of current cycle, apply restarts when necessary."""
         if epoch + 1 == self.next_restart:
             self.batch_since_restart = 0
             self.cycle_length = np.ceil(self.cycle_length * self.mult_factor)
@@ -106,5 +113,73 @@ class SGDRScheduler(Callback):
             self.best_weights = self.model.get_weights()
 
     def on_train_end(self, logs={}):
-        '''Set weights to the values from the end of the most recent cycle for best performance.'''
+        """Set weights to the values from the end of the most recent cycle for best performance."""
         self.model.set_weights(self.best_weights)
+
+
+class lr_log(tf.keras.callbacks.Callback):
+    # def on_train_batch_end(self, batch, log=None):
+    # print("wudi")
+
+    # def on_batch_end(self, batch, logs):
+    # logs.update({"lr": 199})
+    # print("lr:", self.model.optimizer._decayed_lr("float32").numpy())
+
+    def on_epoch_end(self, batch, log={}):
+        # def on_epoch_begin(self, batch, log={}):
+        log.update({"lr": self.model.optimizer._decayed_lr("float32").numpy()})
+        print(
+            "lr_decay:", self.model.optimizer._decayed_lr("float32").numpy(),
+        )
+
+
+def keras2pb(model, model_name):
+    # Convert Keras model to ConcreteFunction
+    full_model = tf.function(lambda x: model(x))
+    full_model = full_model.get_concrete_function(
+        tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype)
+    )
+
+    # Get frozen ConcreteFunction
+    frozen_func = convert_variables_to_constants_v2(full_model)
+    frozen_func.graph.as_graph_def()
+
+    layers = [op.name for op in frozen_func.graph.get_operations()]
+    print("-" * 50)
+    print("Frozen model layers: ")
+    for layer in layers:
+        print(layer)
+
+    print("-" * 50)
+    print("Frozen model inputs: ")
+    print(frozen_func.inputs)
+    print("Frozen model outputs: ")
+    print(frozen_func.outputs)
+
+    # Save frozen graph from frozen ConcreteFunction to hard drive
+    tf.io.write_graph(
+        graph_or_graph_def=frozen_func.graph,
+        logdir="./frozen_models",
+        name=model_name + ".pb",
+        as_text=False,
+    )
+
+
+def get_flops(model_h5_path):
+    tf.compat.v1.reset_default_graph()
+    session = tf.compat.v1.Session()
+    graph = tf.compat.v1.get_default_graph()
+
+    with graph.as_default():
+        with session.as_default():
+            model = tf.keras.models.load_model(model_h5_path)
+
+            run_meta = tf.compat.v1.RunMetadata()
+            opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
+
+            # We use the Keras session graph in the call to the profiler.
+            flops = tf.compat.v1.profiler.profile(
+                graph=graph, run_meta=run_meta, cmd="op", options=opts
+            )
+
+            return flops.total_float_ops
